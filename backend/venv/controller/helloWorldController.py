08cacd6b070f9f2ecef5f09a46ec8f7dc421d1f8
cdceballor <cdceballor@eafit.edu.co>
2022-06-22 15:17:20 -0500

    ADD initial step

---
+++ backend/venv/controller/helloWorldController.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template #Import the class Flask
+from markupsafe import escape #Import the markupsafe module to show values_iter
+from flask import request #Create request with methods
+
+app = Flask(__name__) #Create a Flask instance
+# <>
+@app.route('/') #Router decorator to tell flask what URL go
+def index():
+    return "<p>Hello World</p>" #Return the message
+
+@app.route('/index')
+def welcome():
+    return "<h1>This is a link</h1>" #Return the message
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_login_form()
+
+def do_the_login():
+    return "<h1>You have been logged</h1>"
+
+def show_login_form():
+    return render_template('form.html')
+    # '''
+    #           <form method="POST">
+    #               <div><label>Name: <input type="text" name="name"></label></div>
+    #               <div><label>password: <input type="password" name="password"></label></div>
+    #               <input type="submit" value="Submit">
+    #           </form>'''
